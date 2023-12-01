6ab226e37725147f19af06820d69b9ea72e4bce3
phatnguyen138 <minhphatnguyen6@gmail.com>
2023-12-01 17:06:53 +0700

    Fix: Jenkinsfile

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,4 +1,42 @@
-#!/bin/bash
+#!/bin/bashsource_file=".source"
+echo "" > "$source_file"
+# Read each line from .env and export variables to .source
+while IFS= read -r line; do
+  # Extract key and value from each line
+  key=$(echo "$line" | cut -d '=' -f 1)
+  value=$(echo "$line" | cut -d '=' -f 2-)
+
+  # Check if key and value are not empty
+  if [ -n "$key" ] && [ -n "$value" ]; then
+    echo "export $key=$value" >> "$source_file"
+  fi
+done < "$env_file"
+source "${source_file}"
+docker-compose down -f /home/ec2-user/app/docker-compose.yml || echo "Fail"
+env_file="/home/ec2-user/app/.env"
+source_file="/home/ec2-user/app/.source"
+touch source_file
+
+echo "" > "$source_file"
+
+# Read each line from .env and export variables to .source
+while IFS= read -r line; do
+  # Extract key and value from each line
+  key=$(echo "$line" | cut -d '=' -f 1)
+  value=$(echo "$line" | cut -d '=' -f 2-)
+
+  # Check if key and value are not empty
+  if [ -n "$key" ] && [ -n "$value" ]; then
+    echo "export $key=$value" >> "$source_file"
+  fi
+done < "$env_file"
+
+# Source the generated .source file to export the variables
+source "${source_file}"
+
+# Execute docker-compose down
+docker-compose down -f /home/ec2-user/app/docker-compose.yml || echo "Fail"
+
 source .source
 echo ${ECR_TOKEN} | docker login --username AWS --password-stdin 932782693588.dkr.ecr.ap-southeast-1.amazonaws.com
 docker pull 932782693588.dkr.ecr.ap-southeast-1.amazonaws.com/quote-app:${IMAGE_VERSION}
--- deploy/scripts/predeploy.sh
+++ deploy/scripts/predeploy.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-source_file=".source"
-echo "" > "$source_file"
-# Read each line from .env and export variables to .source
-while IFS= read -r line; do
-  # Extract key and value from each line
-  key=$(echo "$line" | cut -d '=' -f 1)
-  value=$(echo "$line" | cut -d '=' -f 2-)
-
-  # Check if key and value are not empty
-  if [ -n "$key" ] && [ -n "$value" ]; then
-    echo "export $key=$value" >> "$source_file"
-  fi
-done < "$env_file"
-source "${source_file}"
-docker-compose down -f /home/ec2-user/app/docker-compose.yml || echo "Fail"
+echo "Pre-deploy"
