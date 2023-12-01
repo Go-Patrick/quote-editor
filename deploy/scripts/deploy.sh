f381f801d05e0b916aae19f15cce09909f5ebec0
phatnguyen138 <minhphatnguyen6@gmail.com>
2023-12-01 17:11:23 +0700

    Fix: Jenkinsfile

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,24 +1,9 @@
-#!/bin/bashsource_file=".source"
-echo "" > "$source_file"
-# Read each line from .env and export variables to .source
-while IFS= read -r line; do
-  # Extract key and value from each line
-  key=$(echo "$line" | cut -d '=' -f 1)
-  value=$(echo "$line" | cut -d '=' -f 2-)
+#!/bin/bash
 
-  # Check if key and value are not empty
-  if [ -n "$key" ] && [ -n "$value" ]; then
-    echo "export $key=$value" >> "$source_file"
-  fi
-done < "$env_file"
-source "${source_file}"
-docker-compose down -f /home/ec2-user/app/docker-compose.yml || echo "Fail"
 env_file="/home/ec2-user/app/.env"
 source_file="/home/ec2-user/app/.source"
 touch source_file
-
 echo "" > "$source_file"
-
 # Read each line from .env and export variables to .source
 while IFS= read -r line; do
   # Extract key and value from each line
@@ -30,11 +15,7 @@ while IFS= read -r line; do
     echo "export $key=$value" >> "$source_file"
   fi
 done < "$env_file"
-
-# Source the generated .source file to export the variables
 source "${source_file}"
-
-# Execute docker-compose down
 docker-compose down -f /home/ec2-user/app/docker-compose.yml || echo "Fail"
 
 source .source
