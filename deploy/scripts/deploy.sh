01375db605a3bef593c0bddd4c3c80c664e0c8c7
phatnguyen138 <minhphatnguyen6@gmail.com>
2023-12-01 17:24:30 +0700

    Fix: Jenkinsfile

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 env_file="/home/ec2-user/app/.env"
-source_file="/home/ec2-user/app/.source"
-touch source_file
-echo "" > "$source_file"
-# Read each line from .env and export variables to .source
-while IFS= read -r line; do
-  # Extract key and value from each line
-  key=$(echo "$line" | cut -d '=' -f 1)
-  value=$(echo "$line" | cut -d '=' -f 2-)
-
-  # Check if key and value are not empty
-  if [ -n "$key" ] && [ -n "$value" ]; then
-    echo "export $key=$value" >> "$source_file"
-  fi
-done < "$env_file"
-source "${source_file}"
+#source_file="/home/ec2-user/app/.source"
+#touch source_file
+#echo "" > "$source_file"
+## Read each line from .env and export variables to .source
+#while IFS= read -r line; do
+#  # Extract key and value from each line
+#  key=$(echo "$line" | cut -d '=' -f 1)
+#  value=$(echo "$line" | cut -d '=' -f 2-)
+#
+#  # Check if key and value are not empty
+#  if [ -n "$key" ] && [ -n "$value" ]; then
+#    echo "export $key=$value" >> "$source_file"
+#  fi
+#done < "$env_file"
+#source "${source_file}"
 docker-compose down -f /home/ec2-user/app/docker-compose.yml || echo "Fail"
 
 source .source
