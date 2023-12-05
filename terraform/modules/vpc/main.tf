4e31a357ffe13b1210fca05a9febd04f020de9ce
phatnguyen138 <minhphatnguyen6@gmail.com>
2023-12-05 15:15:04 +0700

    Feat: Cable config

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -195,7 +195,7 @@ resource "aws_security_group" "redis_sg" {
     from_port = 6379
     to_port = 6379
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.public_sg.id]
   }
 
   egress {
