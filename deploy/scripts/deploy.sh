d149534ce7aefb23b404ccb87df320e8248edadb
phatnguyen138 <minhphatnguyen6@gmail.com>
2023-12-08 14:01:21 +0700

    dev

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -8,5 +8,5 @@ export AWS_DEFAULT_REGION
 docker-compose -f /home/ec2-user/app/docker-compose.yml down || echo "Fail"
 
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 932782693588.dkr.ecr.ap-southeast-1.amazonaws.com
-docker pull 932782693588.dkr.ecr.ap-southeast-1.amazonaws.com/quote-app:"${IMAGE_VERSION}"
+docker pull 932782693588.dkr.ecr.ap-southeast-1.amazonaws.com/quote-app-default:"${IMAGE_VERSION}"
 docker-compose -f /home/ec2-user/app/docker-compose.yml up -d
